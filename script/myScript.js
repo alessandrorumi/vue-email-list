b4c814bc07531347919e3b44550c82239891eccc
alessandrorumi <rumi.alessandro21@gmail.com>
2023-12-13 16:23:52 +0100

    Feature: Stampa delle mail in Html (ciclo JS)

--- script/myScript.js
+++ script/myScript.js
@@ -5,7 +5,11 @@ createApp({
   data() {
 
     return {
-      
+      mail: 
+          {
+            "success": true,
+            "response": ""
+          }
     }
 
   },
@@ -16,6 +20,14 @@ createApp({
 
   mounted() {
   
+    for (let i = 0; i < 10; i++) {
+      axios.get("https://flynn.boolean.careers/exercises/api/random/mail")
+        .then((risposta) => {
+          // console.log(risposta.data.response)
+          this.mail[`response${i}`] = risposta.data.response;
+        })
+    }
+
   }
 
 }).mount('#app')
